cefbe6edad2d7c66928c76e86863e86a679b32a4
devPavanKalyan <mr.kingkalyan@gmail.com>
2025-10-17 12:09:01 +0530

    Initial commit helenoforyou

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <p className="text-3xl font-bold">This site is under construction.</p>
+    </div>
+  );
+};
+
+export default App;
